12954389a810be4f935f8ad11f5063356a647c13
Aristides Staffieri <aristides.staffieri@stellar.org>
2023-05-24 15:23:22 -0600

    adds payment fee component and base state, adds formatter util

--- src/utils/format.ts
+++ src/utils/format.ts
@@ -1,2 +1,13 @@
+import BigNumber from "bignumber.js";
+
 export const truncateString = (str: string) =>
   str ? `${str.slice(0, 5)}…${str.slice(-5)}` : "";
+
+export const stroopToXlm = (
+  stroops: BigNumber | string | number,
+): BigNumber => {
+  if (stroops instanceof BigNumber) {
+    return stroops.dividedBy(1e7);
+  }
+  return new BigNumber(Number(stroops) / 1e7);
+};
